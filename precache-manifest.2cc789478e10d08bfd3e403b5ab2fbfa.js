d464e400190a8eecf00f83597d82dfe51bd44c6a
Mac <amit@amits-MacBook-Pro.local>
2021-05-22 19:11:45 +0100

    Updates

---
+++ precache-manifest.2cc789478e10d08bfd3e403b5ab2fbfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b89590b9763b17d5742107909c0801",
+    "url": "/coinbook/index.html"
+  },
+  {
+    "revision": "a17c9b43929267951d69",
+    "url": "/coinbook/static/css/main.657c5c8b.chunk.css"
+  },
+  {
+    "revision": "332e5d10133c762162b9",
+    "url": "/coinbook/static/js/2.e277732b.chunk.js"
+  },
+  {
+    "revision": "ffc7b6295f5a16b1f3510dec66026f86",
+    "url": "/coinbook/static/js/2.e277732b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17c9b43929267951d69",
+    "url": "/coinbook/static/js/main.6f2286ae.chunk.js"
+  },
+  {
+    "revision": "690beec7c26cc11b9096",
+    "url": "/coinbook/static/js/runtime-main.db4d3bc2.js"
+  },
+  {
+    "revision": "295c53141974d5a728bcfb3efab9930b",
+    "url": "/coinbook/static/media/logoheader.295c5314.svg"
+  }
+]);
